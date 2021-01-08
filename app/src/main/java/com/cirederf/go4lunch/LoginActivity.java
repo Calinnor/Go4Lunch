882b9144b221c02c2b88238ef6f228483bd8a19b
Calinnor <frederic.hautson@gmail.com>
2021-01-08 15:41:05 +0100

    Add login with google on butto_login_google

--- app/src/main/java/com/cirederf/go4lunch/LoginActivity.java
+++ app/src/main/java/com/cirederf/go4lunch/LoginActivity.java
@@ -2,6 +2,12 @@ package com.cirederf.go4lunch;
 
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
+import butterknife.OnClick;
+
 public class LoginActivity extends BaseActivity {
 
     @Override
@@ -14,4 +20,38 @@ public class LoginActivity extends BaseActivity {
     public int getFragmentLayout() {
         return R.layout.activity_main;
     }
+
+    //FOR DATA
+    // 1 - Identifier for Sign-In Activity
+    private static final int RC_SIGN_IN = 123;
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    @OnClick(R.id.button_login_google)
+    public void onClickLoginButton() {
+        // 3 - Launch Sign-In Activity when user clicked on Login Button
+        this.startSignInActivity();
+    }
+
+
+    // --------------------
+    // NAVIGATION
+    // --------------------
+
+    // 2 - Launch Sign-In Activity
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .setIsSmartLockEnabled(false, true)
+                        //.setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
 }
+
